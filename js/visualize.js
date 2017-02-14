a062ab8cca29bb3aa0169052cde70fa0b3c327ea
Philip Conrad <philipaconrad@gmail.com>
2017-02-13 22:50:34 -0500

    Add dataset-wrangling code from this morning.
    Ricky and I had a hacking session this morning to try to get D3 to load
    our CSV dataset. Our dataset now loads, and we get back a huge array of
    per-row JSON objects.
    
    Is this inefficient? Yes, yes it is.
    Does it work? Yes, yes it does.
    
    Some example code is provided showing how to map functions across the
    dataset, and how to use sets to extract only the unique values.
    
    TODO:
     - Add some kind of animated spinner while the dataset loads.
       Materialize should have something for this.

--- js/visualize.js
+++ js/visualize.js
@@ -52,3 +52,54 @@ function renderTimeline() {
 
 window.addEventListener('resize', renderTimeline);
 renderTimeline();
+
+
+// Initialized when data is loaded.
+dataset = null;
+
+// This function loads our CSV dataset, parsing into per-row JSON objects.
+function load_dataset(callback) {
+    var parseDate = d3.timeParse("%m/%d/%Y");
+    d3.csv("data/historical_nav.csv", function(d) {
+        // Build a JSON object for each row.
+        return {
+            date:                     parseDate(d['Date']),
+            proshares_name:           d['ProShares Name'],
+            ticker:                   d['Ticker'],
+            nav:                     +d['NAV'],
+            prior_nav:               +d['Prior NAV'],
+            nav_change_percent:      +d['NAV Change (%)'],
+            nav_change_dollars:      +d['NAV Change ($)'],
+            shares_outstanding:      +d['Shares Outstanding (000)'],
+            assets_under_management: +d['Assets Under Management']
+        }
+    }, function(error, data) {
+        // Push the resulting list of objects to a global.
+        dataset = data;
+        // If a callback was provided, call it.
+        if (typeof(callback) === "function" && callback) {
+            callback();
+        }
+    });
+}
+
+
+// Function to show unique ProShares/Ticker names.
+function display_proshares() {
+    // Display ProShares names in console.
+    var names = dataset.map(function(d) { return d.proshares_name });
+    // Filter to just the unique values.
+    console.log(d3.set(names).values());
+    // Repeat the process for ticker names.
+    var tickers = dataset.map(function(d) { return d.ticker });
+    console.log(d3.set(tickers).values());
+}
+
+
+// This function runs once the document has loaded/rendered.
+$(document).ready(function () {
+    // Use dumb-but-effective callback chaining to get things done.
+    load_dataset(function (){
+        display_proshares();
+    });
+});

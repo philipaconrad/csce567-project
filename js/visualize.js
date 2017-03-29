55ceab7696a9202f0270ad04b7a3d1f5e8e9da17
Tyler Encke <tylerencke@gmail.com>
2017-03-29 18:23:58 -0400

    Function to create data that will work with Cara's performanceGraph Also added functions for adding and removing ETFs from selectedETFs

--- js/visualize.js
+++ js/visualize.js
@@ -8,14 +8,16 @@
 var allETFs = null;
 
 // Modified as user adds/removes ETFs from the ETF drawer.
-var selectedETFs = null;
+// Array of strings, representing the etf ticker
+var selectedETFs = [];
 
 
 // ----------------------------------------------------------------------------
 // Dataset wrangling
 // ----------------------------------------------------------------------------
 
-var parseDate = d3.timeParse("%m/%d/%Y");
+var parseDate = d3.timeParse("%m/%d/%y");
+var formatDate = d3.timeFormat("%y/%m/%d");
 
 // This function loads our CSV dataset, parsing into per-row JSON objects.
 function loadDataset(callback) {
@@ -60,13 +62,14 @@ function loadDataset(callback) {
 }
 
 // This function loads a specific etf, parsing the arrays properly
+// Recommended callback when adding to selectedETFs so performance won't be loaded till etf data is loaded
 function loadETF(etfTicker, callback) {
     // Upper case Ticker
     etfTicker = etfTicker.toUpperCase();
 
     // Check if ETF already loaded. If so, skip loading again
     if (allETFs.hasOwnProperty(etfTicker)) {
-        if (allETFs[etfTicker].hasOwnProperty('NAV')) {
+        if (allETFs[etfTicker].hasOwnProperty('nav')) {
             // If a callback was provided, call it.
             if (typeof(callback) === "function" && callback) {
                 callback();
@@ -113,6 +116,77 @@ function loadETF(etfTicker, callback) {
 // Miscellaneous functions
 // ----------------------------------------------------------------------------
 
+
+// Added ETF to selected ETFs
+function selectETF(etf) {
+    if (selectedETFs.contains(etf)) {
+        return;
+    }
+
+    // Add to selectedETFs
+    selectedETFs.push(etf);
+
+    // Load the etf data and in the callback render the page again
+    loadETF(etf, function() {
+        render();
+    });
+}
+
+// Remove ETF from selected ETFs
+function removeETF(etf) {
+    if (!selectedETFs.contains(etf)) {
+        return;
+    }
+
+    // Add to selectedETFs
+    var index = selectedETFs.indexOf(etf);
+    selectedETFs.splice(index, 1);
+
+    // Render the page again
+    render();
+}
+
+// Create average data structure for the selected ETFs
+function averageSelectedETFs() {
+
+    // Map of dates and values on each date
+    var dateMap = {};
+    var parseDateBack = d3.timeParse("%y/%m/%d");
+
+    // Loop through each etf
+    selectedETFs.forEach(function (etfKey) {
+        if (allETFs.hasOwnProperty(etfKey)) {
+            var etf = allETFs[etfKey];
+            // For each date in the etf add the date and the value to the map
+            for (var i = 0; i < etf.date.length; i++) {
+                var date = formatDate(etf.date[i]);
+                var nav = etf.nav[i];
+
+                // If date not in the map add the empty array
+                if (!dateMap.hasOwnProperty(date)) {
+                    dateMap[date] = [];
+                }
+                dateMap[date].push(nav);
+            }
+        }
+
+    });
+
+    var dateArray = [];
+    // Sum and get average of each date in the dateMap
+    for (var key in dateMap) {
+        if (dateMap.hasOwnProperty(key)) {
+            var sum = dateMap[key].reduce(function(a, b) { return a + b; });
+            dateArray.unshift({
+                'date': parseDateBack(key),
+                'average': sum / dateMap[key].length
+            });
+        }
+    }
+
+    return dateArray;
+}
+
 // Function to show unique ProShares/Ticker names.
 function displayProshares() {
     // Display ProShares names in console.
@@ -124,6 +198,11 @@ function displayProshares() {
     // ERROR HERE HAD TO COMMENT OUTconsole.log(d3.set(tickers).values());
 }
 
+// Function to render page again
+function render() {
+    displayProshares();
+    displayPerformanceGraph();
+}
 
 // ----------------------------------------------------------------------------
 // Initialization functions
@@ -134,6 +213,6 @@ $(document).ready(function () {
     // Use dumb-but-effective callback chaining to get things done.
     loadDataset(function (){
         console.info(allETFs);
-        displayProshares();
+        render();
     });
 });
